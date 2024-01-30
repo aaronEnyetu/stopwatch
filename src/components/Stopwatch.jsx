3e75c04396d27f49540527219a05b64e9f85dab6
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-29 20:35:04 -0800

    Stopwatch App Complete

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -41,7 +41,17 @@ const Stopwatch = () => {
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
     let minutes = Math.floor(elapsedTime / (1000 * 60) % 60)
     let seconds = Math.floor(elapsedTime / (1000 % 60))
-    let milliseconds = Math.floor((elapsedTime % 1000)/10 )
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10)
+    
+
+    hours = String(hours).padStart(2, '0')
+    minutes = String(minutes).padStart(2, '0')
+    seconds = String(seconds).padStart(2, '0')
+    milliseconds = String(milliseconds).padStart(2, '0')
+    
+    
+    
+    
 
     // return `00:00:00`
     return `${hours}:${minutes}:${seconds}:${milliseconds}`
